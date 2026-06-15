f09b9f4fb3b5e2fdad50df7ddb60e30eb7e11a19
Anthony <anthony_celso@hotmail.com>
2026-06-15 10:29:46 -0300

    ajuste no erroHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,15 @@
+const { isCelebrateError } = require("celebrate");
+
 module.exports = (err, req, res, _next) => {
+  if (isCelebrateError(err)) {
+    return res.status(400).send({
+      message: "Dados inválidos. Verifique os campos preenchidos.",
+    });
+  }
+
   const { statusCode = 500 } = err;
 
   const message = statusCode === 500 ? "Erro interno do servidor" : err.message;
 
-  res.status(statusCode).send({ message });
+  return res.status(statusCode).send({ message });
 };
